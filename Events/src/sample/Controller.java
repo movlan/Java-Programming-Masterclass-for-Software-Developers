fe0ae2146370760dc7549661c4b99473f14a2bbd
Bahram Movlanov <movlan@users.noreply.github.com>
2021-04-08 22:24:25 -0600

    .gitignore added to ignore **/out/ files

---
+++ Events/src/sample/Controller.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    public Button helloButton;
+    @FXML
+    public Button byeButton;
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClick(ActionEvent e) {
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        }
+
+        if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
+    }
+}
