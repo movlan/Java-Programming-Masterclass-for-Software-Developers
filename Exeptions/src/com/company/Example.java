f119e6e2780545f50bc568ef9379ccc8a6d3c20c
Bahram Movlanov <movlan@users.noreply.github.com>
2021-04-26 19:23:42 -0600

    Enf of lecture 228
    - Catching and throwing Exeption

--- Exeptions/src/com/company/Example.java
+++ Exeptions/src/com/company/Example.java
@@ -1,23 +1,42 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
+        float result = divide();
         System.out.println(result);
     }
 
     public static int divide() {
-        int x = getInt();
-        int y = getInt();
+        int x, y;
+        try {
+            x = getInt();
+            y = getInt();
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("No suitable input");
+        }
         System.out.println("x is " + x + " and y is " + y);
-        return x/y;
+        try {
+            return x/y;
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("attempt divide by zero");
+        }
     }
 
     private static int getInt() {
         Scanner s = new Scanner(System.in);
-        System.out.println("Please enter integer ");
-        return s.nextInt();
+        System.out.println("Please enter an integer ");
+        while(true) {
+            try {
+                return s.nextInt();
+            } catch (InputMismatchException e) {
+                // go round again. Read past the end of line in the input
+                s.nextLine();
+                System.out.println("Please enter a number using only the digits 0 thorough 9");
+            }
+        }
     }
 }
