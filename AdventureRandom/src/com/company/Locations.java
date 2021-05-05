356e360f502693ec5429ce2d6ba1dca02255527d
Bahram Movlanov <movlan@users.noreply.github.com>
2021-05-04 19:48:02 -0600

    End of Lecture 242
    - create RandomAccessFile

--- AdventureRandom/src/com/company/Locations.java
+++ AdventureRandom/src/com/company/Locations.java
@@ -5,10 +5,49 @@ import java.util.*;
 
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
+    private static Map<Integer, IndexRecord> index = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
-            rao.write(locations.size());
+            rao.writeInt(locations.size());
+            int indexSize = locations.size() * 3 * Integer.BYTES;
+            int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
+            System.out.println(indexSize);
+            System.out.println(rao.getFilePointer());
+            rao.writeInt(locationStart);
+
+            long indexStart = rao.getFilePointer();
+
+            int starPointer = locationStart;
+            rao.seek(starPointer);
+
+            for (Location location : locations.values()) {
+                rao.writeInt(location.getLocationID());
+                rao.writeUTF(location.getDescription());
+                StringBuilder builder = new StringBuilder();
+                for (String direction : location.getExits().keySet()) {
+                    if (!direction.equalsIgnoreCase("Q")) {
+                        builder.append(direction);
+                        builder.append(",");
+                        builder.append(location.getExits().get(direction));
+                        builder.append(',');
+                    }
+                }
+                rao.writeUTF(builder.toString());
+
+                IndexRecord record = new IndexRecord(starPointer, (int) (rao.getFilePointer() - starPointer));
+                index.put(location.getLocationID(), record);
+
+                starPointer = (int) rao.getFilePointer();
+            }
+
+            rao.seek(indexStart);
+            for (Integer locationID : index.keySet()) {
+                rao.writeInt(locationID);
+                rao.writeInt(index.get(locationID).getStartByte());
+                rao.writeInt(index.get(locationID).getLength());
+            }
+
         }
     }
 
